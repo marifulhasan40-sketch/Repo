9cb37a4ee889616163e81b05ff20a5c9da07a044
marifulhasan40-sketch <marifulhasan40@gmail.com>
2026-05-31 01:55:49 +0600

    Add MainActivity with basic UI setup

---
+++ app/src/main/java/com/mfdplay/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.mfdplay
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = "MFD Play - অ্যাপ সফলভাবে চলছে! 🚀"
+    }
+}
